74cc096d52eaf5d81319889b719bee71c542d679
Fantasy <captainoftheship65@proton.me>
2025-04-11 14:24:56 +0200

    fix: move NODE_ENV to shared

--- src/lib/env.ts
+++ src/lib/env.ts
@@ -2,7 +2,7 @@ import {createEnv} from '@t3-oss/env-nextjs';
 import {z} from 'zod';
 
 export const env = createEnv({
-  server: {
+  shared: {
     NODE_ENV: z
       .enum(['development', 'test', 'production'])
       .default('development'),
@@ -12,6 +12,7 @@ export const env = createEnv({
   },
   experimental__runtimeEnv: {
     NEXT_PUBLIC_APP_URL: process.env.NEXT_PUBLIC_APP_URL,
+    NODE_ENV: process.env.NODE_ENV,
   },
   emptyStringAsUndefined: true,
 });
